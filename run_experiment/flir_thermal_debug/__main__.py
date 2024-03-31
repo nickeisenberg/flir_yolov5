bc623df4d007fec63743039659ea28bd60aa11d3
nickeisenberg <nickeisenberg@gmail.com>
2024-03-30 22:11:49 -0700

    set up the yolo debug. need to run to continue debugging

--- run_experiment/flir_thermal_debug/__main__.py
+++ run_experiment/flir_thermal_debug/__main__.py
@@ -1,22 +1,20 @@
 from sys import path
 path.append(__file__.split("run_experiment")[0])
 
-from config import config
+from config import config_trainer
 from src.trainer.trainer import Trainer
 
+config = config_trainer()
 
 trainer = Trainer(
-    model=config["model"], 
-    loss_fn=config["loss_fn"], 
-    optimizer=config["optimizer"],
-    save_root=config["save_root"],
-    unpacker=config["unpacker"]
-    
+    train_module=config["train_module"],
 )
+
 if __name__ == "__main__":
     trainer.fit(
         train_loader=config["train_loader"],
         num_epochs=config["num_epochs"],
         device=config["device"],
         val_loader=config["val_loader"],
+        unpacker=config["unpacker"]
     )
