b84981a8497ca3cfdf130d6b236a920579e9bfe1
nickeisenberg <nickeisenberg@gmail.com>
2024-04-03 17:55:11 -0700

    config ready for car vs vehical not car

---
+++ run_experiment/car_and_others/__main__.py
@@ -0,0 +1,30 @@
+from sys import path
+
+path.append(__file__.split("run_experiment")[0])
+
+from config import config_trainer
+from src.trainer.trainer import Trainer
+
+config = config_trainer()
+
+if __name__ == "__main__":
+    trainer = Trainer(
+        train_module=config["train_module"],
+    )
+    
+    device = config["device"]
+    if isinstance(device, str) or isinstance(device, int):
+        device = device
+    elif isinstance(device, list):
+        device = device[0]
+    else:
+        raise Exception("device error")
+    
+    assert type(device) == str or type(device) == int
+
+    trainer.fit(
+        train_loader=config["train_loader"],
+        num_epochs=config["num_epochs"],
+        device=device,
+        unpacker=config["unpacker"]
+    )
