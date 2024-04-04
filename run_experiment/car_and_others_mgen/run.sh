d75aeb932877cea05918a3e15460a64d5439f83b
nickeisenberg <nickeisenberg@gmail.com>
2024-04-04 10:37:20 -0700

    updated slurm scrip

--- run_experiment/car_and_others_mgen/run.sh
+++ run_experiment/car_and_others_mgen/run.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 
-#SBATCH --job-name=pytorch_ddp
-#SBATCH --output=%x.o%j
+#SBATCH --job-name=model
+#SBATCH --output=out.%x.o%j.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
 #SBATCH --partition=pvis
 
 srun /g/g11/eisenbnt/venvs/base/bin/python3 \
-	        -u /g/g11/eisenbnt/projects/flir_yolov5/run_experiment/flir_thermal_debug
+	        -u /g/g11/eisenbnt/projects/flir_yolov5/run_experiment/car_and_others_mgen/__main__.py
