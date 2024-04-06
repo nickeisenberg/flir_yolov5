f740e575c8c5fd84bd70af4e6f6c84139ced128d
nickeisenberg <nickeisenberg@gmail.com>
2024-04-06 07:53:32 -0700

    need to rerun bbox5 becuase of logger issue. validation overfit at 60 but I kept saving statedicts. going to run a 10 as well

--- run_experiment/with_aug_bbox15/post_train_eda.py
+++ run_experiment/with_aug_bbox15/post_train_eda.py
@@ -11,11 +11,11 @@ from PIL import Image
 from src.yolo_utils.targets import decode_yolo_output
 from src.yolo_utils.utils import nms, iou
 from src.yolo_utils.box_viewers import (
-    view_boxes_actual,
+    view_pred_vs_actual,
 )
 from src.yolov5.yolov5 import YOLOv5
 
-from run_experiment.with_augmentations.config import (
+from run_experiment.with_aug_bbox15.config import (
     config_coco,
     config_datasets,
     config_train_module_inputs
@@ -45,12 +45,13 @@ sd = torch.load(
 
 yolov5.load_state_dict(sd["MODEL_STATE"])
 
-# img, target = vdataset[225]
+
+ #img, target = vdataset[225]
 # img, target = vdataset[45]
 # img, target = vdataset[105]
-# img, target = vdataset[118]
+img, target = vdataset[118]
 # img, target = vdataset[600]
-img, target = vdataset[701]
+# img, target = vdataset[701]
 img = img.unsqueeze(0)
 prediction = yolov5(img)
 decoded_prediction = decode_yolo_output(
@@ -58,11 +59,24 @@ decoded_prediction = decode_yolo_output(
 )
 pred_box_idxs = nms(decoded_prediction["bboxes"], .3)
 pred_boxes = decoded_prediction["bboxes"][pred_box_idxs]
+pred_labels = decoded_prediction["category_ids"][pred_box_idxs].tolist()
+pred_scores = decoded_prediction["scores"][pred_box_idxs]
 actual = decode_yolo_output(
     tuple(target), img_width, img_height, .3, anchors, scales, False
 )
+actual_boxes = actual["bboxes"]
+actual_labels = actual["category_ids"].tolist()
 pil_img: Image.Image = transforms.ToPILImage()(img[0])
-view_boxes_actual(pil_img, pred_boxes, actual["bboxes"])
+view_pred_vs_actual(
+    pil_img, boxes=pred_boxes.tolist(), 
+    scores=pred_scores.tolist(), 
+    labels=pred_labels, 
+    boxes_actual=actual_boxes.tolist(), 
+    labels_actual=actual_labels
+)
+
+print(actual_labels)
+print(pred_labels)
 
 loss_df = pd.read_csv(os.path.join(loss_log_root, "train_log.csv"))
 loss_df["batch"] = loss_df.index // (272 * 3)
