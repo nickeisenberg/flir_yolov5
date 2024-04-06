0608d0f9965a4e1371bbf5ee2a38dafedf22076b
nickeisenberg <nickeisenberg@gmail.com>
2024-04-06 07:55:57 -0700

    updated slurm scrupts

---
+++ run_experiment/with_aug_bbox10_shuf/post_train_eda.py
@@ -0,0 +1,87 @@
+import os
+from sys import path
+path.append(os.path.expanduser("~/GitRepos/flir_yolov5"))
+import torch
+import torchvision.transforms as transforms
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from PIL import Image
+
+from src.yolo_utils.targets import decode_yolo_output
+from src.yolo_utils.utils import nms, iou
+from src.yolo_utils.box_viewers import (
+    view_pred_vs_actual,
+)
+from src.yolov5.yolov5 import YOLOv5
+
+from run_experiment.with_aug_bbox5.config import (
+    config_coco,
+    config_datasets,
+    config_train_module_inputs
+)
+
+tcoco, vcoco = config_coco()
+
+(
+    in_channels, 
+    num_classes, 
+    img_width, 
+    img_height, 
+    anchors, 
+    scales,
+    loss_log_root,
+    state_dict_root
+) = config_train_module_inputs(tcoco)
+
+tdataset, vdataset = config_datasets(tcoco, vcoco, anchors, scales)
+
+yolov5 = YOLOv5(in_channels, num_classes)
+
+sd = torch.load(
+    os.path.join(state_dict_root, "val_ckp.pth"),
+    map_location="cpu"
+)
+
+yolov5.load_state_dict(sd["MODEL_STATE"])
+
+# img, target = vdataset[225]
+# img, target = vdataset[45]
+# img, target = vdataset[105]
+img, target = vdataset[118]
+# img, target = vdataset[600]
+# img, target = vdataset[703]
+img = img.unsqueeze(0)
+prediction = yolov5(img)
+decoded_prediction = decode_yolo_output(
+    prediction, img_width, img_height, .95, anchors, scales, True
+)
+pred_box_idxs = nms(decoded_prediction["bboxes"], .3)
+pred_boxes = decoded_prediction["bboxes"][pred_box_idxs]
+pred_labels = decoded_prediction["category_ids"][pred_box_idxs].tolist()
+pred_scores = decoded_prediction["scores"][pred_box_idxs]
+actual = decode_yolo_output(
+    tuple(target), img_width, img_height, .3, anchors, scales, False
+)
+actual_boxes = actual["bboxes"]
+actual_labels = actual["category_ids"].tolist()
+pil_img: Image.Image = transforms.ToPILImage()(img[0])
+view_pred_vs_actual(
+    pil_img, boxes=pred_boxes.tolist(), 
+    scores=pred_scores.tolist(), 
+    labels=pred_labels, 
+    boxes_actual=actual_boxes.tolist(), 
+    labels_actual=actual_labels
+)
+
+loss_df = pd.read_csv(os.path.join(loss_log_root, "train_log.csv"))
+loss_df["batch"] = loss_df.index // (272 * 3)
+loss_df = loss_df.groupby("batch").mean()
+loss_df.plot()
+plt.show()
+
+loss_df = pd.read_csv(os.path.join(loss_log_root, "val_log.csv"))
+loss_df["batch"] = loss_df.index // (30 * 3)
+loss_df = loss_df.groupby("batch").mean()
+loss_df.plot()
+plt.show()
---
+++ run_experiment/with_aug_bbox5_shuf/pre_run_eda.py
@@ -0,0 +1,44 @@
+import os
+import json
+import numpy as np
+from src.yolo_utils.coco_transformer import coco_transformer
+from src.yolo_utils.box_statistics import make_coco_stats
+
+flir_root = os.path.expanduser("~/datasets/flir")
+
+path = os.path.join(flir_root, "images_thermal_train", "coco.json")
+with open(path, "r") as oj:
+    coco = json.load(oj)
+
+instructions = {}
+for cat in coco["categories"]:
+    name = cat["name"]
+    if name not in [
+        'car', 
+        'person', 
+        'bike', 
+        'bus', 
+        'other vehicle',
+        'motor', 
+        'truck', 
+    ]:
+        instructions[name] = "ignore"
+
+tcoco = coco_transformer(
+    coco, instructions, (25, 640), (25, 512), (0, 640), (0, 512)
+)
+
+
+stats = make_coco_stats(coco)
+
+counts = sorted({k: stats[k]["count"] for k in stats}.items(), key=lambda x: x[1])[::-1]
+
+for tup in counts:
+    print(tup)
+
+
+for cat in stats:
+    bbox = stats[cat]["bboxes"]
+    print(cat, np.quantile(bbox[:, 2:], [.30], axis=0))
+    print(cat, np.mean(bbox[:, 2:], axis=0))
+    print(cat, np.min(bbox[:, 2:], axis=0))
